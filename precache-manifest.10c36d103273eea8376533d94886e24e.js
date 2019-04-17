c0188485dd1733224fd116b5afc7b5b292c3ec6c
jenna1k <jenna_kim1@hotmail.com>
2019-04-17 19:26:51 +0200

    Updates

---
+++ precache-manifest.10c36d103273eea8376533d94886e24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b0ddcce59be84325319",
+    "url": "/react_api/static/js/runtime~main.d3e109b4.js"
+  },
+  {
+    "revision": "081a370fb63fdcac86d4",
+    "url": "/react_api/static/js/main.57d5b6a8.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_api/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "081a370fb63fdcac86d4",
+    "url": "/react_api/static/css/main.0795a504.chunk.css"
+  },
+  {
+    "revision": "e80aadba260bc13fcadede9e47b89df4",
+    "url": "/react_api/index.html"
+  }
+];
